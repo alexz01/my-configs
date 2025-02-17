145f992bafbae1c945d203e326389ae592c202f1
Alexander Umale <alexander.umale@gmail.com>
2025-02-17 10:50:15 -0500

    ps scripts

---
+++ WindowsPowerShell/profile.ps1
@@ -0,0 +1,21 @@
+function prompt {
+    $e = [char]0x1b;
+    $str  = "$e[0;34mPS$e[0m ";
+    $str += "$e[0;32m$env:USERNAME@$env:COMPUTERNAME$e[0m ";
+    $str += "$e[0;34m$((Get-Location).Path)$e[0m";
+
+    $branch = $(git branch --show-current 2>$null);
+    if($branch -ne $null) {
+        $str += " $e[0;33m($branch)$e[0m";
+    }
+    $str += "`n$e[0;34m$((history).length+1)>$e[0m ";
+    return $str;
+}
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
